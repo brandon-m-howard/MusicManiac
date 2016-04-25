89bb4e54c44bf52849d31da6de590fd500547b87
Brandon Howard <brandon.m.howard@icloud.com>
2016-04-24 20:06:47 -0500

    Adding artwork and some physics

--- Music Maniac/GameScene.swift
+++ Music Maniac/GameScene.swift
@@ -12,7 +12,12 @@ class GameScene: SKScene, BluetoothManagerDelegate {
 
 	var bluetooth: BluetoothManager!
 	var alpaca: SKSpriteNode!
-	var health = 3
+	var ground = [SKSpriteNode]()
+	var scoreLabel: SKLabelNode!
+	let GROUND_BLOCKS = 20
+	let MAX_HEALTH = 3
+	var health = 0
+	var score = 0
 
 	override func didMoveToView(view: SKView) {
 		bluetooth = BluetoothManager()
@@ -24,20 +29,21 @@ class GameScene: SKScene, BluetoothManagerDelegate {
 		addGround()
 		addHealth()
 		addAlpacaToView()
+		addScore()
 	}
 
 	func addGravityToView() {
-		self.physicsWorld.gravity = CGVectorMake(0.0, -2)
+		self.physicsWorld.gravity = CGVectorMake(0.0, -4.9)
 	}
 
 	func addAlpacaToView() {
 		alpaca = SKSpriteNode(imageNamed: "Alpaca")
 		alpaca.xScale = 0.25
 		alpaca.yScale = 0.25
-		alpaca.anchorPoint = CGPointZero
-		alpaca.position = CGPointMake(0, frame.height / 2.75)
-		alpaca.physicsBody = SKPhysicsBody(circleOfRadius: alpaca.size.height / 2.75)
+		alpaca.position = CGPointMake(200, frame.height / 2.75)
+		alpaca.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: alpaca.size.width, height: alpaca.size.height))
 		alpaca.physicsBody?.dynamic = true
+		alpaca.physicsBody?.allowsRotation = false
 		self.addChild(alpaca)
 	}
 
@@ -46,16 +52,36 @@ class GameScene: SKScene, BluetoothManagerDelegate {
 	}
 
 	func addGround() {
-
+		for iBlock in 0..<GROUND_BLOCKS {
+			let block = SKSpriteNode(imageNamed: "Ground")
+			ground.append(block)
+			block.xScale = 1.0
+			block.yScale = 1.0
+			block.position = CGPointMake(0 + CGFloat(iBlock)*block.size.width, 125)
+			block.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: block.size.width, height: block.size.height))
+			block.physicsBody?.dynamic = false
+			self.addChild(block)
+		}
 	}
 
 	func addHealth() {
+		health = MAX_HEALTH
+	}
 
+	func addScore() {
+		scoreLabel = SKLabelNode(fontNamed: "Cassius Garrod")
+		scoreLabel.fontSize = 40
+		scoreLabel.text = "Score:  \(score)"
+		scoreLabel.horizontalAlignmentMode = .Left
+		scoreLabel.verticalAlignmentMode = .Top
+		scoreLabel.position = CGPoint(x: 20, y: self.frame.height - 110)
+
+		self.addChild(scoreLabel)
 	}
 
 	func jump() {
 		alpaca.physicsBody!.velocity = CGVectorMake(0, 0)
-		alpaca.physicsBody!.applyImpulse(CGVectorMake(0, 280))
+		alpaca.physicsBody!.applyImpulse(CGVectorMake(0, 1200))
 	}
 
 	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -67,7 +93,12 @@ class GameScene: SKScene, BluetoothManagerDelegate {
 	}
 
 	override func update(currentTime: CFTimeInterval) {
-		print(currentTime)
+		incrementScore()
+	}
+
+	func incrementScore() {
+		score += 1
+		scoreLabel.text = "Score:  \(score)"
 	}
 
 	
---
+++ Music Maniac/HighScoreViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HighScoreViewController.swift
+//  Music Maniac
+//
+//  Created by Brandon Howard on 4/24/16.
+//  Copyright © 2016 Ticklin' The Ivories. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class HighScoreViewController: UIViewController {
+
+}
